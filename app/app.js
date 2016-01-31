6fe65435ddcf35d83ba2e28312b6dd5e6d3faa07
Ian M <ian@union.io>
2016-01-31 12:46:12 -0800

    plumbing

--- app/app.js
+++ app/app.js
@@ -2,12 +2,33 @@
 (function() {
     var app = angular.module("cheatSheet", ["firebase","ngRoute"]);
 
-    app.controller("SeasonRecaps", function($scope, $firebaseObject) {
-        this.eras = eraData;
+    app.controller("SeasonSelector", function($scope, $firebaseObject) {
+        $scope.eras = eraData;
+    });
+    app.controller("SeasonDetail", function($scope, $firebaseObject) {
+        $scope.recapString = recapString;
     });
 
+    app.config(['$routeProvider',
+      function($routeProvider) {
+        $routeProvider.
+          when('/seasons', {
+            templateUrl: 'app/_partials/seasonSelector.html',
+            controller: 'SeasonSelector'
+          }).
+          when('/seasons/:seasonID', {
+            templateUrl: 'app/_partials/seasonDetail.html',
+            controller: 'SeasonDetail'
+          }).
+          otherwise({
+            redirectTo: '/seasons'
+          });
+      }]);
+
     var eraData = [
-        { name: 'Dead Ball Era', years: '1912—1922' },
-        { name: 'Babe Ruth Years', years: '1923—1938' }
+        { name: 'Dead Ball Era', years: '1912—1922', eraID: 1 },
+        { name: 'Babe Ruth Years', years: '1923—1938', eraID: 2 }
     ];
+
+    var recapString = "Stuff happened!";
 })();
